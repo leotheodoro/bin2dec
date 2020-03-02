b03cc749830ca1a4bbd3e54907b218895b8d2eb4
Leonardo Theodoro <2038128-theodoro.dev@users.noreply.gitlab.com>
2020-03-02 13:43:02 -0300

    removing tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,17 @@
 <template>
   <div id="app">
-    
+    <form-convert></form-convert>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import FormConvert from './components/FormConvert.vue'
 import './assets/css/main.css';
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    FormConvert,
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
